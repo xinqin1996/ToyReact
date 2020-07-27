79b601e0c70b81ed3ba5337cd2d7adba2b6fb749
Tony Gao <gwjlshebei@gmail.com>
2020-07-27 22:25:41 +0800

    创建wrapper，使用vdom完成渲染

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 import ToyReact from "./ToyReact";
 
-// class MyComponent {
-//   render() {
-//     return <div>cool</div>;
-//   }
-//   setAttribute(name, value) {
-//     this[name] = value;
-//   }
+class MyComponent {
+  render() {
+    return <div>cool</div>;
+  }
+  setAttribute(name, value) {
+    this[name] = value;
+  }
+  mountTo(parent) {
+    //   获取vdom
+    let vdom = this.render();
+    // 把虚拟dom挂在到父亲
+    vdom.mountTo(parent);
+  }
+}
+const a = <MyComponent name="tony"></MyComponent>;
 
-//   mountTo(parent) {
-//     //   获取vdom
-//     let vdom = this.render();
-//     vdom.mountTo(parent);
-//   }
-// }
-// const a = <MyComponent name="tony"></MyComponent>;
+ToyReact.render(a, document.getElementById("root"));
 
-// ToyReact.render(a, document.getElementById("root"));
+// const a = (
+//   <div name="tony">
+//     <span>hello</span>
+//     <span>world</span>
+//     <span>!</span>
+//   </div>
+// );
 
-const a = (
-  <div name="tony">
-    <span>hello</span>
-    <span>world</span>
-    <span>!</span>
-  </div>
-);
-
-console.log(a);
-document.getElementById("root").appendChild(a);
+// console.log(a);
+// document.getElementById("root").appendChild(a);
